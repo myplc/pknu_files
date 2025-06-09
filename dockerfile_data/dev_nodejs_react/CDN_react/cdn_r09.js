99649602417d330abf55bc3cf11d164a839bdde1
myplc <plctohmi@gmail.com>
2025-06-09 16:54:53 +0900

    cdn 리액트완료

---
+++ dockerfile_data/dev_nodejs_react/CDN_react/cdn_r09.js
@@ -0,0 +1,9 @@
+const users = [
+    {id:1, title:'제목들1', name: 'John', date: '2025-05-05', count: 23},
+    {id:2, title:'제목들2', name: 'Tom', date: '2024-03-15', count: 123},
+    {id:3, title:'제목들3', name: 'Smith', date: '2025-04-15', count: 233},
+    {id:4, title:'제목들4', name: 'Bill', date: '2025-09-25', count: 231},
+    {id:5, title:'제목들5', name: 'Steve', date: '2025-01-21', count: 231},
+    {id:6, title:'제목들5', name: 'Steve', date: '2025-01-04', count: 21},
+    {id:7, title:'제목들5', name: 'Steve', date: '2025-01-11', count: 23}
+]
